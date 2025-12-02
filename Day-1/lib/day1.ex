15d6ba72f120481e625a9718b284d19bce7f8a46
Andrew Bekhiet <40571928+Andrew-Bekhiet@users.noreply.github.com>
2025-12-02 03:05:46 +0200

    day 1

--- Day-1/lib/day1.ex
+++ Day-1/lib/day1.ex
@@ -34,12 +34,20 @@ defmodule Day1 do
       end
     end)
     |> Enum.reduce({50, 0}, fn r, {dial, result} ->
-      new_dial = abs(dial + r)
-      new_dial = if(new_dial >= 100, do: new_dial - 100, else: new_dial)
+      new_dial = dial + r
 
-      new_result =
-        result +
-          abs(div(new_dial + 100 * sign(new_dial), 100) - div(dial, 100)) - 1
+      count_zeroes =
+        Range.new(dial, new_dial)
+        |> Enum.drop(1)
+        |> Enum.reduce(0, fn n, acc ->
+          if rem(n, 100) == 0 do
+            acc + 1
+          else
+            acc
+          end
+        end)
+
+      new_result = result + count_zeroes
 
       # abs(div(new_dial + 100 * sign(new_dial), 100) - div(dial + 100 * sign(dial), 100)) -
       # 1
@@ -58,6 +66,9 @@ defmodule Day1 do
 
   def bool_to_int(true), do: 1
   def bool_to_int(false), do: 0
+
+  def round_away_from_zero(n) when n >= 0, do: ceil(n)
+  def round_away_from_zero(n), do: floor(n)
 
   def parse_input(use_example) do
     filename = if use_example, do: "example-input.txt", else: "input.txt"
